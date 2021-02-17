bad723d871c11268274c8b8825dc1ce34756de27
Arthur A <aires.arthur.15@gmail.com>
2021-02-16 22:39:35 -0300

    More exercises

---
+++ Loops.py
@@ -0,0 +1,8 @@
+def calcNumber(n):
+    for x in range(n):
+        print(x*x)
+        
+
+if __name__ == '__main__':
+    n = int(input())
+    calcNumber(n)
---
+++ Python: Division.py
@@ -0,0 +1,8 @@
+def calcNumbers(a,b):
+    print(a//b)
+    print(a/b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    calcNumbers(a,b)
